7c59a46b0262d273aef8b28ebc121844a2d84c0d
Rithm <algo@rithm.tech>
2016-07-08 18:56:13 -0400

    Update hackerrank-boiler.java

--- hackerrank-boiler.java
+++ hackerrank-boiler.java
@@ -1,22 +1,23 @@
+
 import java.io.*;
 import java.util.*;
 import java.util.stream.*;
 
 public class Solution {
 	
-	int numOfLines;
+        int numOfLines;
 	
-	public Solution(int num){
-		numOfLines = num;
-	    BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-	    readfile(in);
-	}
+        public Solution(int num){
+	numOfLines = num;
+	BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+	readfile(in);
+	
+    }
 
     public static void main(String[] args) {
         Solution solution = new Solution(1);
     }
     
-
     private void readfile(BufferedReader testCase){
     	if(numOfLines == 0){
     		try(Stream<String> lines = testCase.lines()){
@@ -36,8 +37,14 @@ public class Solution {
             	//int n = Integer.parseInt(z);
             	//while( i < 10) System.out.println(n +" x "+ (++i) +" = "+ n*i);
             });
+    	} else{
+    		
+    		//streamOfFileLines.skip(1).map((m)->m.split("(?!^)")).flatMap(this::getAnswer).forEach(System.out::println);
     	}
     }
-}
+    
+    private Stream<?> getAnswer(String line){
+    	Stream<String> answer = Stream.of(line);
+    	return answer;
     }
 }
