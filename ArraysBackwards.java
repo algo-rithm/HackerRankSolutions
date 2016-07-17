27ab22ec72134ec240317882892a3e875d2b5970
Rithm <algo@rithm.tech>
2016-07-17 16:24:13 -0400

    trimmed to bare

--- ArraysBackwards.java
+++ ArraysBackwards.java
@@ -3,29 +3,17 @@ import java.util.*;
 import java.util.stream.*;
 
 public class Solution{
-    
-    public Solution(){
-        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-        readFile(in);
-    }
-    
+
     public static void main(String[] args){
-        Solution solution = new Solution();
-    }
-    
-    private void readFile(BufferedReader testCase){
-        try(Stream<String> lines = testCase.lines().limit(2);){
-            printResults(lines);
-        }
-    }
-    
-    private void printResults(Stream<String> streamOfFileLines){
-        Optional<String> op = streamOfFileLines.skip(1).findFirst();
-        op.ifPresent((z)->{
-            List<String> list = new ArrayList<>(Arrays.asList(z.split("([ ])")));
-            Collections.reverse(list);
-            String join = list.stream().collect(Collectors.joining(" "));
-            System.out.println(join);
-        });
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        try(Stream<String> lines = in.lines().limit(2);){
+                Optional<String> op = lines.skip(1).findFirst();
+                op.ifPresent((z)->{
+                    List<String> list = new ArrayList<>(Arrays.asList(z.split("([ ])")));
+                    Collections.reverse(list);
+                    String join = list.stream().collect(Collectors.joining(" "));
+                    System.out.println(join);
+                });
+            }
     }
 }
